b9fc6f659fb6c7e3d348a8004506e0dcce274511
Rishabh Shrivastava <rishabhshrivastava7565@gmail.com>
2026-01-02 23:02:02 +0530

    done

---
+++ src/main/java/in/co/rays/project_3/util/HibDataSource.java
@@ -0,0 +1,48 @@
+package in.co.rays.project_3.util;
+
+import java.util.ResourceBundle;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hibernate DataSource is provides the object of session factory and session
+ * 
+ * 
+ * @author Rishabh Shrivastava
+ *
+ */
+public class HibDataSource {
+	private static SessionFactory sessionFactory = null;
+
+	public static SessionFactory getSessionFactory() {
+
+		if (sessionFactory == null) {
+            ResourceBundle rb = ResourceBundle.getBundle("in.co.rays.project_3.bundle.system");
+
+            String jdbcUrl = System.getenv("DATABASE_URL");
+            if (jdbcUrl == null || jdbcUrl.trim().isEmpty()) {
+                jdbcUrl = rb.getString("url");
+            }
+           
+            
+            sessionFactory = new Configuration().configure()
+            		.setProperty("hibernate.connection.url", jdbcUrl).buildSessionFactory();		}
+		return sessionFactory;
+	}
+
+	public static Session getSession() {
+
+		Session session = getSessionFactory().openSession();
+		return session;
+
+	}
+
+	public static void closeSession(Session session) {
+
+		if (session != null) {
+			session.close();
+		}
+	}
+}
