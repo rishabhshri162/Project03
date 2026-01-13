0ec227176481c866f5b1bb9b6d6a9b692d08e9e2
Rishabh Shrivastava <116031457+rishabhshri162@users.noreply.github.com>
2026-01-13 13:38:34 +0530

    Update HibDataSource.java

--- src/main/java/in/co/rays/project_3/util/HibDataSource.java
+++ src/main/java/in/co/rays/project_3/util/HibDataSource.java
@@ -15,10 +15,17 @@ public class HibDataSource {
 	private static SessionFactory sessionFactory = null;
 
 	public static SessionFactory getSessionFactory() {
-
 		if (sessionFactory == null) {
-			sessionFactory = new Configuration().configure().buildSessionFactory();
-		}
+            ResourceBundle rb = ResourceBundle.getBundle("in.co.rays.project_3.bundle.system");
+
+            String jdbcUrl = System.getenv("DATABASE_URL");
+            if (jdbcUrl == null || jdbcUrl.trim().isEmpty()) {
+                jdbcUrl = rb.getString("url");
+            }
+           
+            
+            sessionFactory = new Configuration().configure()
+            		.setProperty("hibernate.connection.url", jdbcUrl).buildSessionFactory();		}
 		return sessionFactory;
 	}
 
@@ -28,6 +35,8 @@ public class HibDataSource {
 		return session;
 
 	}
+	
+	
 
 	public static void closeSession(Session session) {
 
